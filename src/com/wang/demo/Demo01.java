844c461a66c4147064e5fb0218144b7f3bd292b1
wangwenhuihui <751829201@qq.com>
2019-11-11 16:11:45 +0800

    Initial commit

---
+++ src/com/wang/demo/Demo01.java
@@ -0,0 +1,8 @@
+package com.wang.demo;
+
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("good good study");
+        System.out.println("day day up");
+    }
+}
